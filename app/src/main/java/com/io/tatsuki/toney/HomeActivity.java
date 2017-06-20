1177aeff8d21fe33b0c85d449bac5fa4d8bbe903
TatsukiIshijima <tatsuki445@gmail.com>
2017-06-20 22:03:50 +0900

    initialize

---
+++ app/src/main/java/com/io/tatsuki/toney/HomeActivity.java
@@ -0,0 +1,13 @@
+package com.io.tatsuki.toney;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
