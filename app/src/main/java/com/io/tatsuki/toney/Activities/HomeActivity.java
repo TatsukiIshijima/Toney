877e04ea6da86596a32b3466ac694930278bb14e
TatsukiIshijima <tatsuki445@gmail.com>
2017-06-27 22:24:40 +0900

    ツールバー設定

--- app/src/main/java/com/io/tatsuki/toney/Activities/HomeActivity.java
+++ app/src/main/java/com/io/tatsuki/toney/Activities/HomeActivity.java
@@ -2,6 +2,7 @@ package com.io.tatsuki.toney.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.io.tatsuki.toney.R;
 
@@ -11,5 +12,10 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.activity_home_toolbar);
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+        }
     }
 }
