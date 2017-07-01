b0908ecb342a948750d3c936d072b492413a944f
TatsukiIshijima <tatsuki445@gmail.com>
2017-07-01 14:41:09 +0900

    曲ページ用のレイアウトとFragmentクラス作成

---
+++ app/src/main/java/com/io/tatsuki/toney/Fragments/SongFragment.java
@@ -0,0 +1,27 @@
+package com.io.tatsuki.toney.Fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.io.tatsuki.toney.R;
+
+/**
+ * 曲 Framgment
+ */
+
+public class SongFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstance) {
+        super.onCreate(savedInstance);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup viewGroup, Bundle savedInstance) {
+        View songView = inflater.inflate(R.layout.fragment_song, null);
+        return songView;
+    }
+}
