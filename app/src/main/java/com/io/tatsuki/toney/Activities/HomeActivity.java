dee2f9ef342daa2ec9dab69d01305e6fe2e5c8db
TatsukiIshijima <tatsuki445@gmail.com>
2017-07-04 21:59:03 +0900

    BottomSheetの挙動による分岐処理追加

--- app/src/main/java/com/io/tatsuki/toney/Activities/HomeActivity.java
+++ app/src/main/java/com/io/tatsuki/toney/Activities/HomeActivity.java
@@ -2,12 +2,14 @@ package com.io.tatsuki.toney.Activities;
 
 import android.databinding.DataBindingUtil;
 import android.support.annotation.NonNull;
+import android.support.design.widget.BottomSheetBehavior;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.io.tatsuki.toney.Adapters.HomePagerAdapter;
 import com.io.tatsuki.toney.R;
@@ -26,6 +28,7 @@ public class HomeActivity extends AppCompatActivity {
         binding.setHomeViewModel(homeViewModel);
 
         setViews(binding);
+        setBottomSheetBehavior(binding);
     }
 
     /**
@@ -60,4 +63,38 @@ public class HomeActivity extends AppCompatActivity {
             return false;
         }
     };
+
+    /**
+     * BottomSheet挙動
+     * @param binding
+     */
+    private void setBottomSheetBehavior(final ActivityHomeBinding binding) {
+        BottomSheetBehavior bottomSheetBehavior = BottomSheetBehavior.from(binding.bottomSheetLayout.bottomSheetRelativeLayout);
+        bottomSheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                switch (newState) {
+                    // レイアウトの切り替え
+                    case BottomSheetBehavior.STATE_DRAGGING:
+                        break;
+                    case BottomSheetBehavior.STATE_SETTLING:
+                        break;
+                    case BottomSheetBehavior.STATE_EXPANDED:
+                        binding.bottomSheetLayout.bottomSheetRelativeLayout.setBackgroundColor(getColor(R.color.colorPrimaryDark));
+                        break;
+                    case BottomSheetBehavior.STATE_COLLAPSED:
+                        binding.bottomSheetLayout.bottomSheetRelativeLayout.setBackgroundColor(getColor(R.color.colorAccent));
+                        break;
+                    case BottomSheetBehavior.STATE_HIDDEN:
+                        break;
+                }
+
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+
+            }
+        });
+    }
 }
