9aa0b24dc0a7cb1bab339a8e49aca809a2b7b263
TatsukiIshijima <tatsuki445@gmail.com>
2017-07-09 15:43:30 +0900

    SongViewModel作成

---
+++ app/src/main/java/com/io/tatsuki/toney/ViewModels/SongViewModel.java
@@ -0,0 +1,14 @@
+package com.io.tatsuki.toney.ViewModels;
+
+/**
+ * Song ViewModel
+ */
+
+public class SongViewModel {
+
+    private static String TAG = SongViewModel.class.getSimpleName();
+
+    public SongViewModel() {
+
+    }
+}
