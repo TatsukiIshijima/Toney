98e645e42675afad5118e949ea6b398e68214a61
TatsukiIshijima <tatsuki445@gmail.com>
2017-07-10 22:36:52 +0900

    setDownloadImageメソッドの修正と画像サイズ指定

--- app/src/main/java/com/io/tatsuki/toney/Utils/ImageUtil.java
+++ app/src/main/java/com/io/tatsuki/toney/Utils/ImageUtil.java
@@ -19,11 +19,9 @@ public class ImageUtil {
      * 画像をダウンロードしてViewにセットする
      * @param context
      * @param path          画像ファイルパス
-     * @param w             画像の任意の横幅
-     * @param h             画像の任意の縦幅
      * @param imageView     View
      */
-    public static void setDownloadImage(@NonNull Context context, String path, int w, int h, ImageView imageView) {
-        Picasso.with(context).load(new File(path)).resize(w, h).into(imageView);
+    public static void setDownloadImage(@NonNull Context context, String path, ImageView imageView) {
+        Picasso.with(context).load(new File(path)).fit().into(imageView);
     }
 }
