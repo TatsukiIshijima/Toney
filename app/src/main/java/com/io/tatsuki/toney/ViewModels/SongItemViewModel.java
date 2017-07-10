83934a1a5d9323124e71c9633700b7ab11e0ea7f
TatsukiIshijima <tatsuki445@gmail.com>
2017-07-10 21:27:30 +0900

    曲リストの項目に関する設定の修正や追加

--- app/src/main/java/com/io/tatsuki/toney/ViewModels/SongItemViewModel.java
+++ app/src/main/java/com/io/tatsuki/toney/ViewModels/SongItemViewModel.java
@@ -2,6 +2,8 @@ package com.io.tatsuki.toney.ViewModels;
 
 import android.databinding.ObservableField;
 
+import com.io.tatsuki.toney.Models.Song;
+
 /**
  * SongItem ViewModel
  */
@@ -10,10 +12,15 @@ public class SongItemViewModel {
 
     private static final String TAG = SongItemViewModel.class.getSimpleName();
 
-    public ObservableField<String> songTitle = new ObservableField<>();
+    public ObservableField<String> songName = new ObservableField<>();
     public ObservableField<String> songArtist = new ObservableField<>();
 
     public SongItemViewModel() {
 
+    }
+
+    public void setSong(Song song) {
+        songName.set(song.getSongName());
+        songArtist.set(song.getSongArtist());
     }
 }
