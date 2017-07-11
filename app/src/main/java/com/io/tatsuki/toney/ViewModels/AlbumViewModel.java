42d72e7f9f0cec19317b0e2b80d2674f6f23b23c
TatsukiIshijima <tatsuki445@gmail.com>
2017-07-11 21:39:59 +0900

    AlbumViewModelの生成

---
+++ app/src/main/java/com/io/tatsuki/toney/ViewModels/AlbumViewModel.java
@@ -0,0 +1,26 @@
+package com.io.tatsuki.toney.ViewModels;
+
+import android.databinding.ObservableField;
+
+import com.io.tatsuki.toney.Models.Album;
+
+/**
+ * Album ViewModel
+ */
+
+public class AlbumViewModel {
+
+    private static final String TAG = AlbumViewModel.class.getSimpleName();
+
+    public ObservableField<String> albumName = new ObservableField<>();
+    public ObservableField<String> albumArtist = new ObservableField<>();
+
+    public AlbumViewModel() {
+
+    }
+
+    public void setAlbum(Album album) {
+        albumName.set(album.getAlbumName());
+        albumArtist.set(album.getAlbumArtist());
+    }
+}
