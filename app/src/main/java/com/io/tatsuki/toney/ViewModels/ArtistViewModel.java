96fa5fe67c560473aade3e896957908dfa107af1
TatsukiIshijima <tatsuki445@gmail.com>
2017-07-12 21:11:09 +0900

    アーティスト画面のレイアウト修正

--- app/src/main/java/com/io/tatsuki/toney/ViewModels/ArtistViewModel.java
+++ app/src/main/java/com/io/tatsuki/toney/ViewModels/ArtistViewModel.java
@@ -1,21 +1,29 @@
 package com.io.tatsuki.toney.ViewModels;
 
+import android.databinding.ObservableField;
 import android.util.Log;
 import android.view.View;
 
 import com.io.tatsuki.toney.Events.ClickEvent;
+import com.io.tatsuki.toney.Models.Artist;
 
 import org.greenrobot.eventbus.EventBus;
 
 /**
- * アーティスト画面 ViewModel
+ * Artist ViewModel
  */
 
 public class ArtistViewModel {
 
     private static final String TAG = ArtistViewModel.class.getSimpleName();
 
+    public ObservableField<String> artistName = new ObservableField<>();
+
     public ArtistViewModel() {
+    }
+
+    public void setArtist(Artist artist) {
+        artistName.set(artist.getArtistName());
     }
 
     public void onClick(View view) {
