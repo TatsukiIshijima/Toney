6e615db6eb15cc05bf4d4e7364fb49af5472eb13
TatsukiIshijima <tatsuki445@gmail.com>
2017-07-13 23:31:24 +0900

    ボタンクリックイベント追加

--- app/src/main/java/com/io/tatsuki/toney/ViewModels/HomeViewModel.java
+++ app/src/main/java/com/io/tatsuki/toney/ViewModels/HomeViewModel.java
@@ -1,5 +1,6 @@
 package com.io.tatsuki.toney.ViewModels;
 
+import android.util.Log;
 import android.view.View;
 
 /**
@@ -8,4 +9,21 @@ import android.view.View;
 
 public class HomeViewModel {
 
+    private static final String TAG = HomeViewModel.class.getSimpleName();
+
+    public HomeViewModel() {
+
+    }
+
+    public void onClickPlay(View view) {
+        Log.d(TAG, "onClickPlay");
+    }
+
+    public void onClickNext(View view) {
+        Log.d(TAG, "onClickNext");
+    }
+
+    public void onClickPrev(View view) {
+        Log.d(TAG, "onClickPrev");
+    }
 }
