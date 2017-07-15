449654449bafb06aac87d1ce205a907f6c84e720
TatsukiIshijima <tatsuki445@gmail.com>
2017-07-15 14:39:42 +0900

    Event通知のコードクラス作成

---
+++ app/src/main/java/com/io/tatsuki/toney/Utils/EventRequestConstants.java
@@ -0,0 +1,12 @@
+package com.io.tatsuki.toney.Utils;
+
+/**
+ * Event通知に用いるコード
+ */
+
+public class EventRequestConstants {
+
+    public static final String CLICK_SONG = "CLICK_SONG";
+    public static final String CLICK_ARTIST = "CLICK_ARTIST";
+    public static final String CLICK_ALBUM = "CLICK_ALBUM";
+}
