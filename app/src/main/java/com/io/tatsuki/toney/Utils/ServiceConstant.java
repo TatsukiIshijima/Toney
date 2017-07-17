20747aa000862fdcaf6eca772891a39be0bfbd4b
TatsukiIshijima <tatsuki445@gmail.com>
2017-07-17 10:49:20 +0900

    Serviceに用いる定数クラス作成

---
+++ app/src/main/java/com/io/tatsuki/toney/Utils/ServiceConstant.java
@@ -0,0 +1,11 @@
+package com.io.tatsuki.toney.Utils;
+
+/**
+ * Serviceに使用するKey名
+ */
+
+public class ServiceConstant {
+
+    public static final int NOTIFICATION_ID = 1;
+    public static final String SERVICE_STOP = "SERVICE_STOP";
+}
