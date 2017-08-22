81a3e4c01624bb617a63c87adedbe9ee14bd3bb3
TatsukiIshijima <tatsuki445@gmail.com>
2017-08-22 21:18:46 +0900

    lombokの書き方に修正

--- app/src/main/java/com/io/tatsuki/toney/Models/Album.java
+++ app/src/main/java/com/io/tatsuki/toney/Models/Album.java
@@ -1,7 +1,10 @@
 package com.io.tatsuki.toney.Models;
 
 import java.io.Serializable;
-import java.util.ArrayList;
+
+
+import lombok.Getter;
+import lombok.Setter;
 
 /**
  * Album Model
@@ -9,58 +12,23 @@ import java.util.ArrayList;
 
 public class Album implements Serializable {
 
+    @Getter
+    @Setter
     private String albumId;
+
+    @Getter
+    @Setter
     private String albumKey;
+
+    @Getter
+    @Setter
     private String albumName;
+
+    @Getter
+    @Setter
     private String albumArtPath;
+
+    @Getter
+    @Setter
     private String albumArtist;
-    private ArrayList<Song> songs;
-
-    public String getAlbumId() {
-        return albumId;
-    }
-
-    public void setAlbumId(String albumId) {
-        this.albumId = albumId;
-    }
-
-    public String getAlbumKey() {
-        return albumKey;
-    }
-
-    public void setAlbumKey(String albumKey) {
-        this.albumKey = albumKey;
-    }
-
-    public String getAlbumName() {
-        return albumName;
-    }
-
-    public void setAlbumName(String albumName) {
-        this.albumName = albumName;
-    }
-
-    public String getAlbumArtPath() {
-        return albumArtPath;
-    }
-
-    public void setAlbumArtPath(String albumArtPath) {
-        this.albumArtPath = albumArtPath;
-    }
-
-    public String getAlbumArtist() {
-        return albumArtist;
-    }
-
-    public void setAlbumArtist(String albumArtist) {
-        this.albumArtist = albumArtist;
-    }
-
-    public ArrayList<Song> getSongs() {
-        return songs;
-    }
-
-    public void setSongs(ArrayList<Song> songs) {
-        this.songs = songs;
-    }
 }
