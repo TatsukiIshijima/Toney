cb65c05c0c357794cace91301fdded62257e9a31
TatsukiIshijima <tatsuki445@gmail.com>
2017-08-22 20:31:57 +0900

    lombok追加とGetter,Setterの省略

--- app/src/main/java/com/io/tatsuki/toney/Models/Artist.java
+++ app/src/main/java/com/io/tatsuki/toney/Models/Artist.java
@@ -3,64 +3,28 @@ package com.io.tatsuki.toney.Models;
 import java.io.Serializable;
 import java.util.ArrayList;
 
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * Artist Model
  */
 
 public class Artist implements Serializable {
 
+    @Getter
+    @Setter
     private String ArtistId;
+
+    @Getter
+    @Setter
     private String ArtistKey;
+
+    @Getter
+    @Setter
     private String ArtistName;
+
+    @Getter
+    @Setter
     private int albumNum;
-    private ArrayList<Album> albums;
-    private ArrayList<Song> songs;
-
-    public String getArtistId() {
-        return ArtistId;
-    }
-
-    public void setArtistId(String artistId) {
-        ArtistId = artistId;
-    }
-
-    public String getArtistKey() {
-        return ArtistKey;
-    }
-
-    public void setArtistKey(String artistKey) {
-        ArtistKey = artistKey;
-    }
-
-    public String getArtistName() {
-        return ArtistName;
-    }
-
-    public void setArtistName(String artistName) {
-        ArtistName = artistName;
-    }
-
-    public int getAlbumNum() {
-        return albumNum;
-    }
-
-    public void setAlbumNum(int albumNum) {
-        this.albumNum = albumNum;
-    }
-
-    public ArrayList<Album> getAlbums() {
-        return albums;
-    }
-
-    public void setAlbums(ArrayList<Album> albums) {
-        this.albums = albums;
-    }
-
-    public ArrayList<Song> getSongs() {
-        return songs;
-    }
-
-    public void setSongs(ArrayList<Song> songs) {
-        this.songs = songs;
-    }
 }
