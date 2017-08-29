5b91d60cae161e736f009a7c7ad53c71860cf93d
TatsukiIshijima <tatsuki445@gmail.com>
2017-08-29 20:03:30 +0900

    ライセンス画面追加

---
+++ app/src/main/java/com/io/tatsuki/toney/Activities/LicenseActivity.java
@@ -0,0 +1,37 @@
+package com.io.tatsuki.toney.Activities;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.io.tatsuki.toney.R;
+
+/**
+ * ライセンス画面
+ */
+public class LicenseActivity extends AppCompatActivity {
+
+    public static Intent startIntent(@NonNull Activity activity) {
+        Intent intent = new Intent(activity, LicenseActivity.class);
+        return intent;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_license);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.activity_license_toolbar);
+        toolbar.setTitle("ライセンス");
+        setSupportActionBar(toolbar);
+
+        WebView webView = (WebView) findViewById(R.id.activity_license_webView);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://www.google.com/");
+    }
+}
